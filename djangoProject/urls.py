a49e68179e10db232116da39731c008350bf1682
borriskiz <borisnechaev2004@yandex.ru>
2024-10-14 22:08:44 +0500

    started views

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from handmade import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path('',views.TestView.as_view(),name='test_view'),
 ]
--- handmade/views.py
+++ handmade/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.http import HttpResponse
 
-# Create your views here.
+from handmade.models import HandmadeItem
+
+class HandmadeItemListView(ListView):
+    model = HandmadeItem
+    template_name = 'handmade/handmade_item_list.html'
+    context_object_name = 'items'
+
+class HandmadeItemDetailView(DetailView):
+    model = HandmadeItem
+    template_name = 'handmade/handmade_item_detail.html'
+    context_object_name = 'item'
+
+
+class HandmadeItemCreateView(CreateView):
+    model = HandmadeItem
+    template_name = 'handmade/handmade_item_form.html'
+    fields = ['name', 'date_receipt', 'item_type', 'materials', 'market', 'price', 'image']
+    def get_success_url(self):
+        return reverse_lazy('handmade_item_detail', kwargs={'pk': self.object.pk})
+
+
+class HandmadeItemUpdateView(UpdateView):
+    model = HandmadeItem
+    template_name = 'handmade/handmade_item_form.html'
+    success_url = reverse_lazy('handmade_item_list')
+
+class HandmadeItemDeleteView(DeleteView):
+    model = HandmadeItem
+    template_name = 'handmade/handmade_item_confirm_delete.html'
+    success_url = reverse_lazy('handmade_item_list')
