cc4495562be79992bc9e1b9ef2f5f12bec538e17
borriskiz <borisnechaev2004@yandex.ru>
2024-10-19 12:15:34 +0500

    filters original

--- handmade/filters.py
+++ handmade/filters.py
@@ -1,21 +1,27 @@
 import django_filters
-
+from django.db.models import Q
 from handmade import models
-
+from handmade.models import ItemType
 
 class HandmadeFilterSet(django_filters.FilterSet):
     price_range = django_filters.RangeFilter(lookup_expr='icontains', field_name='price', label='Цена от и до')
-    name = django_filters.CharFilter(lookup_expr='icontains', field_name='name', label='Название предмета')
     original = django_filters.BooleanFilter(method='filter_original', field_name='originality',
-                                            label='Собственного произовдства')
+                                            label='Собственного производства')
+    term = django_filters.CharFilter(method='filter_term', label='Поиск')
+    item_type = django_filters.ModelChoiceFilter(queryset=ItemType.objects.all(), field_name='item_type',
+                                                 label='Тип предмета')
 
     class Meta:
         model = models.HandmadeItem
-        fields = ['name', 'price_range', 'item_type']
+        fields = ['term', 'item_type', 'price_range', 'original']
 
     def filter_original(self, queryset, name, value):
         if value is None:
             return queryset
-        if value:
-            return queryset.filter(item_type__is_bought='bought')
-        return queryset.filter(item_type__is_bought="not_bought")
+        return queryset.filter(originality='bought' if value else 'not_bought')
+
+    def filter_term(self, queryset,name, value):
+        criteria = Q()
+        for term in value.split():
+            criteria |= Q(name__icontains=term) | Q(item_type__name__icontains=term)
+        return queryset.filter(criteria).distinct()
