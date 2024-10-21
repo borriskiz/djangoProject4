32b8ca4a51ce0f3797b3fc8065dae0be6bc22edc
borriskiz <borisnechaev2004@yandex.ru>
2024-10-21 20:40:16 +0500

    made normal fabrics

--- handmade/factories.py
+++ handmade/factories.py
@@ -1,12 +1,14 @@
 import factory
 
-from models import HandmadeItem, Customer, ItemType,Market,Material
+from handmade.models import HandmadeItem, ItemType, Market, Material
+
 
 class ItemTypeFactory(factory.django.DjangoModelFactory):
     name = factory.Faker('word')
 
     class Meta:
         model = ItemType
+
 
 class MaterialFactory(factory.django.DjangoModelFactory):
     name = factory.Faker('word')
@@ -16,6 +18,7 @@ class MaterialFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Material
 
+
 class MarketFactory(factory.django.DjangoModelFactory):
     name = factory.Faker('company')
     location = factory.Faker('address')
@@ -24,24 +27,20 @@ class MarketFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Market
 
+
 class HandmadeItemFactory(factory.django.DjangoModelFactory):
     name = factory.Faker('sentence')
     date_receipt = factory.Faker('date')
     item_type = factory.SubFactory(ItemTypeFactory)
-    materials = factory.List([factory.SubFactory(MaterialFactory) for _ in range(3)])  # Создает 3 материала
     market = factory.SubFactory(MarketFactory)
     price = factory.Faker('random_int', min=1, max=1000)
-    image = factory.django.ImageField(color='blue')  # Генерирует заглушку изображения
+    image = factory.django.ImageField(color='blue')
+
+    materials = factory.RelatedFactoryList(
+        MaterialFactory,
+        factory_related_name='handmadeitem',
+        size=3
+    )
 
     class Meta:
         model = HandmadeItem
-
-class CustomerFactory(factory.django.DjangoModelFactory):
-    first_name = factory.Faker('first_name')
-    last_name = factory.Faker('last_name')
-    email = factory.Faker('email')
-    phone = factory.Faker('phone_number')
-    items = factory.List([factory.SubFactory(HandmadeItemFactory) for _ in range(2)])  # Создает 2 HandmadeItem
-
-    class Meta:
-        model = Customer
--- handmade/tests.py
+++ handmade/tests.py
@@ -1,3 +1,52 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from handmade import factories, models
+
+
+class HandmadeTestCase(TestCase):
+    def setUp(self):
+        self.item = factories.HandmadeItemFactory()
+
+    def test_get_item_list(self):
+        url = reverse('handmade_item_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['items'].count(), models.HandmadeItem.objects.count())
+
+    def test_detail_view(self):
+         url = reverse('handmade_item_detail', kwargs={'pk': self.item.pk})
+         response = self.client.get(url)
+         self.assertEqual(response.status_code, 200)
+
+    def test_delete_view(self):
+         url = reverse('handmade_item_delete', kwargs={'pk': self.item.pk})
+         old_items_count = models.HandmadeItem.objects.count()
+         response = self.client.delete(url)
+         self.assertEqual(response.status_code, 302)
+         self.assertGreater(old_items_count, models.HandmadeItem.objects.count())
+
+    def test_update_view(self):
+        url = reverse('handmade_item_update', kwargs={'pk': self.item.pk})
+        old_name = self.item.name
+
+
+        data = {
+            'name': 'new_name',
+            'price': self.item.price,
+            'item_type': self.item.item_type.pk,
+            'market': self.item.market.pk,
+            'date_receipt': self.item.date_receipt,
+            'image': self.item.image,
+            'materials': [factories.MaterialFactory().pk, factories.MaterialFactory().pk],
+        }
+
+        response = self.client.post(url, data)
+
+        if response.status_code == 200:
+            print(response.context['form'].errors)
+
+        self.item.refresh_from_db()
+
+        self.assertEqual(response.status_code, 302)
+        self.assertNotEqual(self.item.name, old_name)
